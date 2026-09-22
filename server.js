3a6fcbdb57e53e223e7e0f4b5921b651708cd169
ogtroxin <troxinlub@gmail.com>
2026-09-22 10:21:33 +0000

    Update server.js

--- server.js
+++ server.js
@@ -10,10 +10,6 @@ const __dirname = path.dirname(__filename);
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
-
-app.listen(port, () => {
+app.listen(port, "0.0.0.0", () => {
   console.log(`RIZZBOT running on port ${port}`);
 });
